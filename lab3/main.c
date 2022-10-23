2669088a51c6ad5b090ab100932d7ae4a573297f
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2022-10-23 09:32:58 -0400

    first idea of implementation %xK

--- lab3/main.c
+++ lab3/main.c
@@ -3,11 +3,24 @@
 
 int my_printf(char *format_string, char *param){
 	for(int i=0;i<strlen(format_string);i++){
-		if((format_string[i] == '#') && (format_string[i+1] == '.') && (format_string[i+3] == 'k') && isdigit(format_string[i+2])){
-			char vartable[5];
-			sprintf(vartable, "%%.%cs", format_string[i+2]);
-			printf(vartable,param);
-			i+=3;
+		if((format_string[i] == '#')){
+			printf("test");
+			if ((format_string[i+1] == '.') && isdigit(format_string[i+2]) && (format_string[i+3] == 'k')){
+				char vartable[5];
+				sprintf(vartable, "%%.%cs", format_string[i+2]);
+				printf(vartable,param);
+				i+=3;
+			}
+			if (isdigit(format_string[i+1]) != 0){
+				int num = 0;
+				while (format_string[i+num] != 'k'){
+					num++;
+				}
+				char tab[num];
+				strncat(format_string[i+1],tab,num);
+				int x = atoi(tab);
+				printf("%d",x);
+			}
 		}else
 			if (format_string[i] >= 65 && format_string[i] <= 90){
 				putchar(format_string[i]+32);
