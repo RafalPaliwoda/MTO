83d3068d7b8a58f8eedefe9ab992f0e52b4ad77c
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2022-12-11 08:25:04 -0500

    add first part of implementation

--- lab6/main.py
+++ lab6/main.py
@@ -5,10 +5,29 @@ import sys
 def my_printf(format_string,param):
     #print(format_string)
     shouldDo=True
+    skip_next=False
     for idx in range(0,len(format_string)):
+        if skip_next:
+            skip_next=False
+            continue
         if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
+            if format_string[idx] == '#':
+                x=0
+                text=''
+                while True:
+                    if format_string[idx+x] == 'g':
+                        break
+                    str = format_string[idx+x].isdigit()
+                    if str:
+                        text+=format_string[idx+x]
+                    x+=1
+                leng = len(param)
+                text = int(text)
+                while text > leng:
+                    print("9", end="")
+                    leng+=1
+                print(param[:text],end="")
+                skip_next=True
                 shouldDo=False
             else:
                 print(format_string[idx],end="")
