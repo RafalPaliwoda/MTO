b7c62f19591e7dd4629ba08a1c11c78f40a031d1
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2022-12-18 08:34:40 -0500

    add changing signs

--- lab7/main.py
+++ lab7/main.py
@@ -10,7 +10,24 @@ def my_printf(format_string,param):
             if format_string[idx] == '#' and format_string[idx+1] == 'j':
                 new_text = int(param)
                 new_text = hex(new_text)
-                print(new_text,end="")
+                new_text = str(new_text)
+                changed_text=''
+                for sign in new_text:
+                    if sign == 'a':
+                        changed_text+='g'
+                    elif sign == 'b':
+                        changed_text+='h'
+                    elif sign == 'c':
+                        changed_text+='i'
+                    elif sign == 'd':
+                        changed_text+='j'
+                    elif sign == 'e':
+                        changed_text+='k'
+                    elif sign == 'f':
+                        changed_text+='l'
+                    else:
+                        changed_text+=sign
+                print(changed_text,end="")
                 shouldDo=False
             else:
                 print(format_string[idx],end="")
