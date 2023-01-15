32bf66285f4b4ce82e12d03195bd951ec7d3209e
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2023-01-15 08:23:42 -0500

    update implementation

--- lab8/main.py
+++ lab8/main.py
@@ -3,9 +3,18 @@
 import sys
 
 def my_printf(format_string,param):
-    #print(format_string)
+    shouldDo=True
+    skip_next=False
+    skip_one_more=False
     shouldDo=True
     for idx in range(0,len(format_string)):
+        if skip_next:
+            skip_next=False
+            skip_one_more=True
+            continue
+        if skip_one_more:
+            skip_one_more=False
+            continue
         if shouldDo:
             if format_string[idx] == '#' and format_string[idx+1] == '.' and format_string[idx+2].isdigit():
                 x=2
@@ -37,7 +46,10 @@ def my_printf(format_string,param):
                         changed_text+='l'
                     else:
                         changed_text+=sign
+                changed_text = changed_text[2:]
+                print(number)
                 print(changed_text,end="")
+                skip_next=True
                 shouldDo=False
             else:
                 print(format_string[idx],end="")
